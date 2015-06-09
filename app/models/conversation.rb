46678bc7a3ea678ab1e1980faf02c1015696538f
Jacob Powers <powersjcb@gmail.com>
2015-06-09 13:39:03 -0700

    Add conversation table

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: conversations
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Conversation < ActiveRecord::Base
+
+  validates :title, :user_id, presence: true
+
+  belongs_to :creator, class_name: "User"
+  has_many :messages
+  # has_many :subscriptions
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609194655) do
+ActiveRecord::Schema.define(version: 20150609203317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "conversations", ["title"], name: "index_conversations_on_title", using: :btree
+  add_index "conversations", ["user_id"], name: "index_conversations_on_user_id", using: :btree
 
   create_table "messages", force: :cascade do |t|
     t.text     "content",         null: false
