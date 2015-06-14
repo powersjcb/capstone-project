8c2f4c44f9fe62bb3f3a4906ad2b9837c2a7f7cd
Jacob Powers <powersjcb@gmail.com>
2015-06-13 21:55:32 -0700

    touch file

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -3,10 +3,5 @@ Slick.Models.User = Backbone.Model.extend({
 
 
   conversations: function () {
-    // if (this._conversations) {
-    //   return this._conversations;
-    // }
-    // this._conversations = new Slick.Collections.Conversations({user: this});
-    // return this._conversations;
   }
 });
