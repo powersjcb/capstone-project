350666b4c44dfe1b72ddc0d2b7dbb27733ff73e9
Jacob Powers <powersjcb@gmail.com>
2015-06-14 23:34:27 -0700

    Ran message callback migration

--- db/migrate/20150615063035_add_callbackid_to_messages.rb
+++ db/migrate/20150615063035_add_callbackid_to_messages.rb
@@ -1,4 +1,5 @@
 class AddCallbackidToMessages < ActiveRecord::Migration
   def change
+    add_column :messages, :callback_id, :string, null: false, default: ""
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612225056) do
+ActiveRecord::Schema.define(version: 20150615063035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,11 +51,12 @@ ActiveRecord::Schema.define(version: 20150612225056) do
   add_index "memberships", ["user_id"], name: "index_memberships_on_user_id", using: :btree
 
   create_table "messages", force: :cascade do |t|
-    t.text     "content",         null: false
-    t.integer  "sender_id",       null: false
-    t.integer  "conversation_id", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.text     "content",                      null: false
+    t.integer  "sender_id",                    null: false
+    t.integer  "conversation_id",              null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.string   "callback_id",     default: "", null: false
   end
 
   add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
